a05fbb2c36947d452861e55edf0f7aac781bb643
Vsevolod Migdisov <vmigdisov@icloud.com>
2026-04-16 16:14:16 +0300

    binary earch refactoring: naming, run tests

--- Algorithms.playground/Pages/2 Binary Search.xcplaygroundpage/Contents.swift
+++ Algorithms.playground/Pages/2 Binary Search.xcplaygroundpage/Contents.swift
@@ -6,16 +6,15 @@
 func findRecursively(in array: ArraySlice<Int>, for value: Int) -> Int? {
     guard array.count > 0 else { return nil }
     
-    let middleIndex = array.count / 2
-    let middleSliceIndex = array.startIndex + middleIndex
+    let middleIndex = array.startIndex + array.count / 2
     
-    switch array[middleSliceIndex] {
+    switch array[middleIndex] {
     case let middleValue where middleValue > value:
-        return findRecursively(in: array.prefix(middleIndex), for: value)
+        return findRecursively(in: array.prefix(upTo: middleIndex), for: value)
     case let middleValue where middleValue < value:
-        return findRecursively(in: array.suffix(from: middleSliceIndex + 1), for: value)
+        return findRecursively(in: array.suffix(from: middleIndex + 1), for: value)
     default:
-        return middleSliceIndex
+        return middleIndex
     }
 }
 
@@ -27,14 +26,21 @@ func findIteratively(in array: Array<Int>, for value: Int) -> Int {
 }
 
 /*:
-## Let's try some tests:
+## Testing:
+ 
+ let's define variables
 */
-    let itemsRange = 0..<20 // number of items in array
-    let valuesRange = 0..<1000 // item values range
-    let array = itemsRange.map { _ in Int.random(in: valuesRange) }.sorted() // a sorted array
-    let indexToFind = Int.random(in: itemsRange) // index to be find by a value
-    let valueToFind = array[indexToFind] // a value to be find in the index
-    let foundIndex = findRecursively(in: array[...], for: valueToFind) ?? -1 // run recursive binary search
-    print("Recursive binary search algorithm is correct:", foundIndex == indexToFind) // check the result
-
+    let itemsRange = 0..<20 /// number of items in array
+    let valuesRange = 0..<1000 /// item values range
+    let array = itemsRange.map { _ in Int.random(in: valuesRange) }.sorted() /// a sorted array
+    let indexToFind = Int.random(in: itemsRange) /// index to be find by a value
+    let valueToFind = array[indexToFind] /// a value to be find in the index
+    let testsRange = 0..<10000 /// number of tests
+/*:
+ let's run a seria of tests
+ */
+    let recursiveResult = testsRange.allSatisfy { _ in
+        findRecursively(in: array[...], for: valueToFind) == indexToFind
+    }
+    print("Recursive binary search algorithm is correct:", recursiveResult)
 //: [< Previous](@previous)                    [Home](1%20Home)                    [Next >](@next)
